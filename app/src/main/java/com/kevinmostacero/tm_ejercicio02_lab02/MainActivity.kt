1455340c130c132b679d77319cb67dadae5f321a
kevmoslin <kevinmostacerolinares@gmail.com>
2026-03-25 10:36:13 -0500

    me olvide del main

--- app/src/main/java/com/kevinmostacero/tm_ejercicio02_lab02/MainActivity.kt
+++ app/src/main/java/com/kevinmostacero/tm_ejercicio02_lab02/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.kevinmostacero.tm_ejercicio02_lab02
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.GridView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var gridViewPizzas: GridView
+    private lateinit var listaPizzas: ArrayList<Pizza>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        gridViewPizzas = findViewById(R.id.gridViewPizzas)
+
+        listaPizzas = arrayListOf(
+            Pizza("Hawaiana", R.drawable.pizza_hawaiana),
+            Pizza("Pepperoni", R.drawable.pizza_pepperoni),
+            Pizza("Americana", R.drawable.pizza_americana),
+            Pizza("Vegetariana", R.drawable.pizza_vegetariana),
+            Pizza("Cuatro Quesos", R.drawable.pizza_cuatro_quesos),
+            Pizza("Suprema", R.drawable.pizza_suprema)
+        )
+
+        val adapter = PizzaAdapter(this, listaPizzas)
+        gridViewPizzas.adapter = adapter
+
+        gridViewPizzas.setOnItemClickListener { _, _, position, _ ->
+            Toast.makeText(
+                this,
+                "Pizza: ${listaPizzas[position].nombre}",
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 }
